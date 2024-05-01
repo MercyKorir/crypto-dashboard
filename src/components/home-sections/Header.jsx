6c0fd7df493c545ddbfa36c54a5ec26fee86a99d
MercyKorir <mcthegreat78@gmail.com>
2024-05-01 23:00:38 +0300

    Deleted unwanted files

--- src/components/home-sections/Header.jsx
+++ src/components/home-sections/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import styles from "../../styles/home-sections/Header.module.css";
 
 const Header = () => {
   const [username, setUsername] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userName = localStorage.getItem("userName");
@@ -11,11 +13,16 @@ const Header = () => {
       setUsername(userName);
     }
   }, []);
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   const userInitial = username ? username.charAt(0).toUpperCase() : "";
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <div className={styles.backArrow}>
+        <div className={styles.backArrow} onClick={handleGoBack}>
           <ArrowBackIcon className={styles.backArrowIcon} fontSize="inherit" />
         </div>
         <div className={styles.userProfile}>
