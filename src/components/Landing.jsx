a7fd9bf6311d137313f4d0f231d77259b57b43d2
MercyKorir <mcthegreat78@gmail.com>
2024-05-01 21:57:00 +0300

    Feat: Landing Page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import LoadingAnimation from "./LoadingAnimation";
 import styles from "../styles/Landing.module.css";
 
@@ -26,7 +27,9 @@ const Landing = () => {
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.logo}>Crypto</div>
-        <Link to={"/signin"}>Login</Link>
+        <Link to={"/signin"} className={styles.loginBtn}>
+          Login
+        </Link>
       </header>
       <div className={styles.hero}>
         <div className={styles.heroContent}>
@@ -40,13 +43,15 @@ const Landing = () => {
 
       <footer className={styles.footer}>
         <div className={styles.footerContent}>
-          <div className={styles.footerLogo}>Logo</div>
+          <div className={styles.footerLogo}>Crypto</div>
           <div className={styles.footerSocial}>
-            <Link to={""}>GitHub</Link>
+            <Link to={"https://github.com/MercyKorir"}>
+              <GitHubIcon fontSize="inherit" />
+            </Link>
           </div>
         </div>
         <div className={styles.footerCopyright}>
-          <p>&copy; 2023 Your Company. All rights reserved.</p>
+          <p>&copy; 2024. All rights reserved.</p>
         </div>
       </footer>
     </div>
