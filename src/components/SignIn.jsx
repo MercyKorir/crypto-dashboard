5917d20be1e0f53c46cbf1c72eb92509c0ee7fa2
MercyKorir <mcthegreat78@gmail.com>
2024-05-01 13:12:01 +0300

    Implemented form validation

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,18 +3,69 @@ import { Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
-import styles from "../styles/SignIn.module.css";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import styles from "../styles/SignIn.module.css";
 
 const SignIn = () => {
   const [emailFocus, setEmailFocus] = useState(false);
   const [passwordFocus, setPasswordFocus] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState({});
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const validateEmail = () => {
+    let emailErrors = {};
+    if (!email.trim()) {
+      emailErrors.email = "Email is required!";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      emailErrors.email = "Email is invalid!";
+    }
+
+    if (Object.keys(emailErrors).length === 0) {
+      setError((prevErrors) => {
+        delete prevErrors.email;
+        return { ...prevErrors };
+      });
+    } else {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        ...emailErrors,
+      }));
+    }
+  };
+
+  const validatePassword = () => {
+    let passwordErrors = {};
+    if (!password) {
+      passwordErrors.password = "Password is required!";
+    }
+
+    if (Object.keys(passwordErrors).length === 0) {
+      setError((prevErrors) => {
+        delete prevErrors.password;
+        return { ...prevErrors };
+      });
+    } else {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        ...passwordErrors,
+      }));
+    }
+  };
+
+  const handleEmailBlur = () => {
+    validateEmail();
+    setEmailFocus(false);
+  };
+
+  const handlePwdBlur = () => {
+    validatePassword();
+    setPasswordFocus(false);
+  };
 
   const handleEmailFocus = () => {
     setEmailFocus(true);
@@ -35,7 +86,7 @@ const SignIn = () => {
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
-      setError(`Error: ${errorCode} - ${errorMessage}`);
+      setError({ firebaseError: `Error: ${errorCode} - ${errorMessage}` });
       console.log(errorCode, errorMessage);
     }
   };
@@ -45,49 +96,75 @@ const SignIn = () => {
       <form onSubmit={handleSubmit}>
         <div className={styles.inputContainer}>
           <div className={styles.inputItem}>
-            <span className={emailFocus ? styles.hideIcon : ""}>
-              <EmailIcon className={styles.formIcon} />
-            </span>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              onFocus={handleEmailFocus}
-              onBlur={() => setEmailFocus(false)}
-              required
-            />
-            <label
-              htmlFor="email"
-              className={emailFocus ? styles.hideLabel : ""}
+            <div>
+              <span className={emailFocus ? styles.hideIcon : ""}>
+                <EmailIcon className={styles.formIcon} />
+              </span>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onFocus={handleEmailFocus}
+                onBlur={handleEmailBlur}
+                required
+              />
+              <label
+                htmlFor="email"
+                className={emailFocus ? styles.hideLabel : ""}
+              >
+                Email
+              </label>
+            </div>
+            <div
+              className={`${
+                error.email ? styles.validationErr : styles.hideValidationErr
+              }`}
             >
-              Email
-            </label>
+              <span className={styles.warnIcon}>
+                <WarningAmberIcon fontSize="inherit" />
+              </span>{" "}
+              <p>{error.email}</p>
+            </div>
           </div>
           <div className={styles.inputItem}>
-            <span className={passwordFocus ? styles.hideIcon : ""}>
-              <LockIcon className={styles.formIcon} />
-            </span>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              onFocus={handlePasswordFocus}
-              onBlur={() => setPasswordFocus(false)}
-              required
-            />
-            <label
-              htmlFor="password"
-              className={passwordFocus ? styles.hideLabel : ""}
+            <div>
+              <span className={passwordFocus ? styles.hideIcon : ""}>
+                <LockIcon className={styles.formIcon} />
+              </span>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onFocus={handlePasswordFocus}
+                onBlur={handlePwdBlur}
+                required
+              />
+              <label
+                htmlFor="password"
+                className={passwordFocus ? styles.hideLabel : ""}
+              >
+                Password
+              </label>
+            </div>
+            <div
+              className={`${
+                error.password ? styles.validationErr : styles.hideValidationErr
+              }`}
             >
-              Password
-            </label>
+              <span className={styles.warnIcon}>
+                <WarningAmberIcon fontSize="inherit" />
+              </span>{" "}
+              <p>{error.password}</p>
+            </div>
           </div>
         </div>
-        {error && <div className={styles.errorMessage}>{error}</div>}
+        {error.firebaseError && (
+          <div className={styles.errorMessage}>{error.firebaseError}</div>
+        )}
         <div className={styles.navigationContainer}>
           <div className={styles.forgotPwdContainer}>
             <Link className={styles.forgotPwd} to={"/"}>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,6 +6,7 @@ import styles from "../styles/SignUp.module.css";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 
 const SignUp = () => {
   const [emailFocus, setEmailFocus] = useState(false);
@@ -17,9 +18,107 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [confPassword, setConfPassword] = useState("");
   const [username, setUsername] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState({});
 
   const navigate = useNavigate();
+
+  const validateEmail = () => {
+    let emailErrors = {};
+    if (!email.trim()) {
+      emailErrors.email = "Email is required!";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      emailErrors.email = "Email is invalid!";
+    }
+
+    if (Object.keys(emailErrors).length === 0) {
+      setError((prevErrors) => {
+        delete prevErrors.email;
+        return { ...prevErrors };
+      });
+    } else {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        ...emailErrors,
+      }));
+    }
+  };
+
+  const validatePassword = () => {
+    let passwordErrors = {};
+    if (!password) {
+      passwordErrors.password = "Password is required!";
+    }
+
+    if (Object.keys(passwordErrors).length === 0) {
+      setError((prevErrors) => {
+        delete prevErrors.password;
+        return { ...prevErrors };
+      });
+    } else {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        ...passwordErrors,
+      }));
+    }
+  };
+
+  const validateConfPassword = () => {
+    let confPasswordErrors = {};
+    if (!confPassword) {
+      confPasswordErrors.confPassword = "Password is required!";
+    }
+
+    if (Object.keys(confPasswordErrors).length === 0) {
+      setError((prevErrors) => {
+        delete prevErrors.confPassword;
+        return { ...prevErrors };
+      });
+    } else {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        ...confPasswordErrors,
+      }));
+    }
+  };
+
+  const validateUsername = () => {
+    let usernameErrors = {};
+    if (!username.trim()) {
+      usernameErrors.username = "Username is required!";
+    }
+
+    if (Object.keys(usernameErrors).length === 0) {
+      setError((prevErrors) => {
+        delete prevErrors.username;
+        return { ...prevErrors };
+      });
+    } else {
+      setError((prevErrors) => ({
+        ...prevErrors,
+        ...usernameErrors,
+      }));
+    }
+  };
+
+  const handleEmailBlur = () => {
+    validateEmail();
+    setEmailFocus(false);
+  };
+
+  const handlePwdBlur = () => {
+    validatePassword();
+    setPasswordFocus(false);
+  };
+
+  const handleConfPwdBlur = () => {
+    validateConfPassword();
+    setConfPasswordFocus(false);
+  };
+
+  const handleUsernameBlur = () => {
+    validateUsername();
+    setUsernameFocus(false);
+  };
 
   const handleEmailFocus = () => {
     setEmailFocus(true);
@@ -60,7 +159,7 @@ const SignUp = () => {
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
-      setError(`${errorCode}: ${errorMessage}`);
+      setError({ firebaseError: `${errorCode}: ${errorMessage}` });
       console.log(errorCode, errorMessage);
     }
   };
@@ -70,91 +169,143 @@ const SignUp = () => {
       <form onSubmit={handleSubmit}>
         <div className={styles.inputContainer}>
           <div className={styles.inputItem}>
-            <span className={usernameFocus ? styles.hideIcon : ""}>
-              <AccountCircleIcon className={styles.formIcon} />
-            </span>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              onFocus={handleUsernameFocus}
-              onBlur={() => setUsernameFocus(false)}
-              required
-            />
-            <label
-              htmlFor="username"
-              className={usernameFocus ? styles.hideLabel : ""}
+            <div>
+              <span className={usernameFocus ? styles.hideIcon : ""}>
+                <AccountCircleIcon className={styles.formIcon} />
+              </span>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                onFocus={handleUsernameFocus}
+                onBlur={handleUsernameBlur}
+                required
+              />
+              <label
+                htmlFor="username"
+                className={usernameFocus ? styles.hideLabel : ""}
+              >
+                Username
+              </label>
+            </div>
+            <div
+              className={`${
+                error.username ? styles.validationErr : styles.hideValidationErr
+              }`}
             >
-              Username
-            </label>
+              <span className={styles.warnIcon}>
+                <WarningAmberIcon fontSize="inherit" />
+              </span>{" "}
+              <p>{error.username}</p>
+            </div>
           </div>
           <div className={styles.inputItem}>
-            <span className={emailFocus ? styles.hideIcon : ""}>
-              <EmailIcon className={styles.formIcon} />
-            </span>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              onFocus={handleEmailFocus}
-              onBlur={() => setEmailFocus(false)}
-              required
-            />
-            <label
-              htmlFor="email"
-              className={emailFocus ? styles.hideLabel : ""}
+            <div>
+              <span className={emailFocus ? styles.hideIcon : ""}>
+                <EmailIcon className={styles.formIcon} />
+              </span>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onFocus={handleEmailFocus}
+                onBlur={handleEmailBlur}
+                required
+              />
+              <label
+                htmlFor="email"
+                className={emailFocus ? styles.hideLabel : ""}
+              >
+                Email
+              </label>
+            </div>
+            <div
+              className={`${
+                error.email ? styles.validationErr : styles.hideValidationErr
+              }`}
             >
-              Email
-            </label>
+              <span className={styles.warnIcon}>
+                <WarningAmberIcon fontSize="inherit" />
+              </span>{" "}
+              <p>{error.email}</p>
+            </div>
           </div>
           <div className={styles.inputItem}>
-            <span className={passwordFocus ? styles.hideIcon : ""}>
-              <LockIcon className={styles.formIcon} />
-            </span>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              onFocus={handlePasswordFocus}
-              onBlur={() => setPasswordFocus(false)}
-              required
-            />
-            <label
-              htmlFor="password"
-              className={passwordFocus ? styles.hideLabel : ""}
+            <div>
+              <span className={passwordFocus ? styles.hideIcon : ""}>
+                <LockIcon className={styles.formIcon} />
+              </span>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onFocus={handlePasswordFocus}
+                onBlur={handlePwdBlur}
+                required
+              />
+              <label
+                htmlFor="password"
+                className={passwordFocus ? styles.hideLabel : ""}
+              >
+                Password
+              </label>
+            </div>
+            <div
+              className={`${
+                error.password ? styles.validationErr : styles.hideValidationErr
+              }`}
             >
-              Password
-            </label>
+              <span className={styles.warnIcon}>
+                <WarningAmberIcon fontSize="inherit" />
+              </span>{" "}
+              <p>{error.password}</p>
+            </div>
           </div>
           <div className={styles.inputItem}>
-            <span className={confPasswordFocus ? styles.hideIcon : ""}>
-              <LockIcon className={styles.formIcon} />
-            </span>
-            <input
-              type="password"
-              id="confirm"
-              name="confirm"
-              value={confPassword}
-              onChange={(e) => setConfPassword(e.target.value)}
-              onFocus={handleConfPasswordFocus}
-              onBlur={() => setConfPasswordFocus(false)}
-              required
-            />
-            <label
-              htmlFor="confirm"
-              className={confPasswordFocus ? styles.hideLabel : ""}
+            <div>
+              <span className={confPasswordFocus ? styles.hideIcon : ""}>
+                <LockIcon className={styles.formIcon} />
+              </span>
+              <input
+                type="password"
+                id="confirm"
+                name="confirm"
+                value={confPassword}
+                onChange={(e) => setConfPassword(e.target.value)}
+                onFocus={handleConfPasswordFocus}
+                onBlur={handleConfPwdBlur}
+                required
+              />
+              <label
+                htmlFor="confirm"
+                className={confPasswordFocus ? styles.hideLabel : ""}
+              >
+                Confirm Password
+              </label>
+            </div>
+            <div
+              className={`${
+                error.confPassword
+                  ? styles.validationErr
+                  : styles.hideValidationErr
+              }`}
             >
-              Confirm Password
-            </label>
+              <span className={styles.warnIcon}>
+                <WarningAmberIcon fontSize="inherit" />
+              </span>{" "}
+              <p>{error.confPassword}</p>
+            </div>
           </div>
         </div>
-        {error && <div className={styles.errorMessage}>{error}</div>}
+        {error.firebaseError && (
+          <div className={styles.errorMessage}>{error.firebaseError}</div>
+        )}
         <div className={styles.navigationContainer}>
           <div className={styles.forgotPwdContainer}>
             <Link className={styles.forgotPwd} to={"/"}>
